7b757c1b3a8ab2d2842b2010bd7c5e19551f5380
Tiger <tigercui@umich.edu>
2026-03-14 10:08:13 -0400

    revised get comments

--- backend/app/reddit_client.py
+++ backend/app/reddit_client.py
@@ -2,6 +2,25 @@ import requests
 
 HEADERS = {"User-Agent": "authentic-food-assistant"}
 
+def get_comments(post_url):
+    url = post_url + ".json"
+
+    try:
+        response = requests.get(url, headers=HEADERS, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+    except Exception:
+        return []
+    
+    comments = []
+
+    for comment in data[1]["data"]["children"]:
+        if comment["kind"]== "t1":
+            body = comment["data"]["body"]
+            comments.append(body)
+        
+    return comments
+
 def search_reddit(query, limit = 5):
     url = f"https://www.reddit.com/search.json?q={query}&limit={limit}"
 
@@ -12,25 +31,16 @@ def search_reddit(query, limit = 5):
 
     for post in data["data"]["children"]:
         p = post["data"]
+
+        comments = get_comments(p["url"])
+
         posts.append({
             "title": p["title"],
             "subreddit": p["subreddit"],
             "score": p["score"],
-            "text": p.get("selftxt",""),
-            "url": p["url"]
+            "comments": comments[:5],
+            "url": f"https://www.reddit.com{p['permalink']}"
         })
     
     return posts
 
-def get_comments(post_url):
-    url = post_url + ".json"
-
-    response = requests.get(url, headers = HEADERS)
-    data = response.json()
-
-    comments = []
-
-    for comment in data[1]["data"]["children"]:
-        if comment["kind"]== "t1":
-            body = comment["data"]["body"]
-    
